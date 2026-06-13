a8ca5423bbe93d7845e1e5fa1fcc4e41549fd21d
wyattjones-wcu <wyattjones-wcu@users.noreply.github.com>
2026-06-13 16:20:19 -0400

    Add testing sheet table

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const pageLinks = document.querySelectorAll("[data-page-link]");
 const pages = document.querySelectorAll("[data-page]");
 const tabs = document.querySelectorAll("[data-tab]");
 const tabPanels = document.querySelectorAll("[data-tab-panel]");
+const testingPlayerRows = document.querySelector("#testing-player-rows");
 
 function showPage(pageName) {
   pages.forEach((page) => {
@@ -48,8 +49,56 @@ showPage(window.location.hash.replace("#", "") || "results");
 loadSiteData()
   .then((data) => {
     window.boxThisLapData = data;
+    renderTestingPlayers(data.players);
     console.info("Box This Lap data loaded", data);
   })
   .catch((error) => {
+    renderTestingError(error);
     console.error("Box This Lap data failed to load", error);
   });
+
+function renderTestingPlayers(players) {
+  if (!testingPlayerRows) {
+    return;
+  }
+
+  if (players.length === 0) {
+    testingPlayerRows.innerHTML = `<tr><td colspan="7">No player data found.</td></tr>`;
+    return;
+  }
+
+  testingPlayerRows.innerHTML = players.map((player) => {
+    return `
+      <tr>
+        <td>${escapeHtml(player.id)}</td>
+        <td>${escapeHtml(player.name)}</td>
+        <td>${escapeHtml(player.team)}</td>
+        <td>${escapeHtml(player.position)}</td>
+        <td>${escapeHtml(player.playerNumber)}</td>
+        <td>${escapeHtml(player.transfermarktPrice)}</td>
+        <td>${escapeHtml(player.drafted)}</td>
+      </tr>
+    `;
+  }).join("");
+}
+
+function renderTestingError(error) {
+  if (!testingPlayerRows) {
+    return;
+  }
+
+  testingPlayerRows.innerHTML = `
+    <tr>
+      <td colspan="7">Unable to load Google Sheets data: ${escapeHtml(error.message)}</td>
+    </tr>
+  `;
+}
+
+function escapeHtml(value) {
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
